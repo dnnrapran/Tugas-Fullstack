c4f93b7a4f7ccdd015dee0445fe2f7b908117710
ftiannisa <firstiannisa.02@gmail.com>
2022-12-08 02:30:26 +0700

    delete workflows

--- With PHP/backend/databaseConnect.php
+++ With PHP/backend/databaseConnect.php
@@ -1,19 +1,9 @@
 <?php
 
-// require __DIR__.'/vendor/autoload.php';
-
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
-
 $host = "localhost"; // if the variable is found then return the variable host else returns the other key
 $dbUsername = "root";
 $dbPassword = "";
 $dbname = "apotek";
-// $host = $_ENV['host']; // if the variable is found then return the variable host else returns the other key
-// $dbUsername = $_ENV['dbUsername'];
-// $dbPassword = $_ENV['dbPassword'];
-// $dbname = $_ENV['dbname'];
-
 
     //create connection
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
